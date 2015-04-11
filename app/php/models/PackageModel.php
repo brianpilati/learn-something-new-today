731b50e229de40bed366a6b02c00f2c03e0bb568
Brian Pilati <brianpilati@gmail.com>
2015-04-11 10:50:22 -0600

    updates the packages query to include items

--- app/php/models/PackageModel.php
+++ app/php/models/PackageModel.php
@@ -16,12 +16,17 @@ class PackageModel extends db {
                 c.categoryId as categoryId,
                 c.category as category,
                 p.class as class,
-                p.family as family
+                p.family as family,
+                i.item as item
             FROM 
                 package p,
-                category c
+                category c,
+                packageConnector pc,
+                item i
             WHERE
                 p.categoryId = c.categoryId
+                AND p.packageId = pc.packageId
+                AND pc.itemId = i.itemId
             ORDER BY
                 c.category, p.class, p.family
         ;
