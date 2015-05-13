a30545bf61649e25275a824c9306be67ed146d44
Brian Pilati <bpilati@insidesales.com>
2015-05-12 22:39:03 -0600

    additional work on tests for CTA

--- test/php/unit/CTAPackageTest.php
+++ test/php/unit/CTAPackageTest.php
@@ -13,5 +13,10 @@ class CTAPackageTest extends PHPUnit_Framework_TestCase
     public function testGetPackages() 
     {
         $this->assertEquals(sizeof($this->ctaPackages->getPackages()), 2);
+        $packages = $this->ctaPackages->getPackages();
+        $package = $packages[0];
+        $this->assertEquals($package->getTitle(), 'Top Five Star Wars Sets');
+        $package = $packages[1];
+        $this->assertEquals($package->getTitle(), '2015 Toyota Vehicles');
     }
 }
